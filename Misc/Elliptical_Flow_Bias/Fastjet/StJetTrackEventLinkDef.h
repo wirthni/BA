86e521e43e08d0c17a9223fe9eccdc6741f2dd75
Nicolas Wirth <nico.wirth_02@gmx.de>
2026-08-14 16:42:59 +0200

    Q vector analysis works really now

--- Misc/Elliptical_Flow_Bias/Fastjet/StJetTrackEventLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ nestedclass;
-#pragma link C++ class StEMCal+;
-#pragma link C++ class StJetTrackParticle+;
-#pragma link C++ class StJetTrackEvent+;
-#pragma link C++ class StMCParticle+;
-
-#endif
--- Misc/HistoMerger/PythiaEvent_LinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ nestedclass;
-#pragma link C++ class PythiaParticle+;
-#pragma link C++ class PythiaEvent+;
-
-
-#endif
